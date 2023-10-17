75f69ec336641b59dd7dcb0110600ceaf8f4e73c
Boniface <Cheruiyotbony@gmail.com>
2023-10-17 21:55:05 +0300

    completed assignement

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+
+function submitData(userName,userEmail)
+{
+  const data={
+    name:userName,
+    email:userEmail
+  }
+
+
+  const postData=
+  {
+    method:'POST',
+    headers:
+    {
+      "content-Type":"application/json",
+      Accept:"application/json",
+    },
+    body:JSON.stringify(data)
+  }
+
+
+  return fetch("http://localhost:3000/users",postData).then(function (response) {
+    return response.json();
+}).then (function(data){
+  document.querySelector('body').textContent=data.id
+})
+    .catch((err) => {
+        document.querySelector("body").textContent = err.message;
+      
+
+
+});}
